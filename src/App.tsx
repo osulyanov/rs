e974af0b442e89e257e9d459bc7a7861f530ba8e
Oleg Sulyanov <oleg@sulyanov.com>
2025-02-02 22:39:14 +0700

    feat: add page layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
+const ansiiHeader = `   ______          _      __
+  / __/ /____ ____| | /| / ___ ________
+ _\\ \\/ __/ _ \`/ __| |/ |/ / _ \`/ __(_-<
+/_______/\\_,_/_/  |__/|__/\\_,_/_/ /___/
+  / _____ ___ ____(____ ___
+ _\\ \\/ _ / -_/ __/ / -_(_-<
+/___/ .__\\__/\\__/_/\\__/___/
+   /_/`;
+
 function App() {
-  const [count, setCount] = useState(0);
+  const dataData = `
++-------+-----------------------------------------------+
+| Name  |                  Description                  |
++-------+-----------------------------------------------+
+| Human | classification: mammal, designation: sentient |
++-------+-----------------------------------------------+`;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      <pre className="ascii-header">{ansiiHeader}</pre>
+      <div className="input-group">
+        <input
+          type="text"
+          className="input-field"
+          placeholder="[ENTER LOCATION]"
+        />
+        <button onClick={() => console.log('fetch')} className="fetch-button">
+          &gt; SCAN
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <pre className="data-report hidden">{dataData}</pre>
+    </div>
   );
 }
 
