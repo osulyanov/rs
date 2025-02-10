a22474081287515fb104ca334a2a75c99d041424
Oleg Sulyanov <oleg@sulyanov.com>
2025-02-11 00:36:34 +0700

    feat: configure vitest

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      include: ['**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+      ],
+      thresholds: {
+        lines: 70,
+      },
+    },
+  },
+});
