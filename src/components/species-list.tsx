45d007ed6cbf537020230a7bc3627983b8ed7fee
Oleg Sulyanov <oleg@sulyanov.com>
2025-02-10 11:29:25 +0700

    feat: add link to a specie

--- src/components/species-list.tsx
+++ src/components/species-list.tsx
@@ -1,7 +1,7 @@
 import { SpeciesResult } from '../utils/fetch-species-list.tsx';
 import { AsciiTable3 } from 'ascii-table3';
 import MessageBox from './message-box.tsx';
-import Specie from './specie.tsx';
+import SpecieListItem from './specie-list-item.tsx';
 
 interface SpeciesListProps {
   speciesList: SpeciesResult[] | null;
@@ -30,13 +30,15 @@ function SpeciesList({ speciesList, loadingState }: SpeciesListProps) {
         (speciesList.length === 0 ? (
           <MessageBox message="NO SPECIES FOUND" />
         ) : (
-          <pre className="data-report">
-            {speciesTableHeader}
-            {speciesList.map((specie, index) => {
-              return <Specie key={index} specie={specie} />;
-            })}
-            {speciesTableEnd}
-          </pre>
+          <>
+            <pre className="data-report">
+              {speciesTableHeader}
+              {speciesList.map((specie, index) => {
+                return <SpecieListItem key={index} specie={specie} />;
+              })}
+              {speciesTableEnd}
+            </pre>
+          </>
         ))}
     </>
   );
