8b730f6aa10fd34ef8beab96391d0fcb63b83554
Oleg Sulyanov <oleg@sulyanov.com>
2025-02-10 11:30:20 +0700

    feat: add index, list and detail routes, 404

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,14 +4,53 @@ import ErrorBoundary from './error-boundary.tsx';
 import Header from './header.tsx';
 import AppCrashingButton from '../components/app-crashing-button.tsx';
 import MessageBox from '../components/message-box.tsx';
+import { NavLink, Outlet, Route, Routes } from 'react-router';
 
-function App() {
+function Layout() {
   return (
     <div className="container">
       <Header />
-      <SpeciesLookup />
+      <Outlet />
       <AppCrashingButton />
     </div>
+  );
+}
+
+function SpecieDetail() {
+  return (
+    <div className="specie-detail">
+      <h1>Specie Detail</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <MessageBox
+      message={
+        <>
+          NOT FOUND |{' '}
+          <NavLink to="/" className="link">
+            GO HOME
+          </NavLink>
+        </>
+      }
+    />
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<SpeciesLookup />} />
+        <Route path="/species" element={<SpeciesLookup />}>
+          <Route path=":specieId" element={<SpecieDetail />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
