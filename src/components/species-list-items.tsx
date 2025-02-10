15334d831a99df34fdb6918ce90c9cee61ad62e1
Oleg Sulyanov <oleg@sulyanov.com>
2025-02-10 23:21:06 +0700

    refactor: use standard .active class for active links

--- src/components/species-list-items.tsx
+++ src/components/species-list-items.tsx
@@ -1,20 +1,11 @@
 import { SpeciesResult } from '../utils/fetch-species-list.tsx';
-import {
-  NavLink,
-  NavLinkRenderProps,
-  Outlet,
-  useNavigate,
-  useSearchParams,
-} from 'react-router';
+import { NavLink, Outlet, useNavigate, useSearchParams } from 'react-router';
 
 interface SpeciesListItemsProps {
   speciesList: SpeciesResult[];
 }
 
 function SpeciesListItems({ speciesList }: SpeciesListItemsProps) {
-  const linkStyle = ({ isActive }: NavLinkRenderProps) => ({
-    textDecoration: isActive ? 'underline' : undefined,
-  });
   const [searchParams] = useSearchParams();
   const page = searchParams.get('page');
   const term = searchParams.get('term');
@@ -35,8 +26,6 @@ function SpeciesListItems({ speciesList }: SpeciesListItemsProps) {
           <div key={index} className="cell">
             <NavLink
               to={`/species/${specie.url.split('/').slice(-2)[0]}?page=${page}${term ? `&term=${term} ` : ''}`}
-              style={linkStyle}
-              className={'glitch-hover'}
               onClick={handleLinkClick}
             >
               <strong>{specie.name}.</strong> (class: {specie.classification},
