4d55cba29c887befdfbf977dcdbd323c6c495a16
Oleg Sulyanov <oleg@sulyanov.com>
2025-02-10 13:24:40 +0700

    feat: delete app crashing button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 import Header from './header.tsx';
 import { Outlet } from 'react-router';
-import AppCrashingButton from '../components/app-crashing-button.tsx';
 
 function Layout() {
   return (
     <div className="container">
       <Header />
       <Outlet />
-      <AppCrashingButton />
     </div>
   );
 }
