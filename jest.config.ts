9d57e08afefbd7aa24418e078e2df25db9acad10
Oleg Sulyanov <oleg@sulyanov.com>
2025-02-11 10:25:18 +0700

    feat: configure jest

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    // process `*.tsx` files with `ts-jest`
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/support/setupTests.ts'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 70,
+    },
+  },
+  collectCoverageFrom: ['src/**/*.tsx'],
+};
